c429fb5cb2668fb1ebe3fdd5ce5ba421353362f9
KapilBansal320 <kapilbansal.gbpecdelhi@gmail.com>
2020-08-01 02:34:09 +0530

    Digest Cycle and Apply

--- app.js
+++ app.js
@@ -77,4 +77,66 @@ function TruthFilter(){
 	};
 }
 
+angular.module('CounterApp', [])
+.controller('CounterController',CounterController);
+
+CounterController.$inject=["$scope",'$timeout'];
+
+function CounterController($scope){
+	// $scope.onceCounter = 0;
+	$scope.counter = 0;
+	// $scope.name="Kapil";
+
+	// $scope.showWatchers=function(){
+	// 	console.log("no. of watchers",$scope.$$watchersCount);
+	// };
+
+	// $scope.countOnce= function(){
+	// 	$scope.onceCounter=1;
+	// };
+
+	$scope.upCounter =function(){
+		$timeout(function(){
+			$scope.counter++;
+			console.log("Counter incremented!");
+		},2000);
+	};
+
+	// $scope.upCounter =function(){
+	// 	setTimeout(function(){
+	// 		$scope.$apply(function(){
+	// 			$scope.counter++;
+	// 			console.log("Counter incremented!");
+	// 		});
+	// 	},2000);
+	// };
+
+	// Why apply -- So that any exceptions thrown are also visible to angular
+	// $scope.upCounter =function(){
+	// 	setTimeout(function(){
+	// 		$scope.counter++;
+	// 		console.log("Counter incremented!");
+	// 		$scope.$digest(); // So that the digest cycle is called in first time too
+	// 	},2000);
+	// };
+
+	// $scope.upCounter =function(){
+	// 	$scope.counter++
+	// };
+
+	// $scope.$watch(function(){
+	// 	console.log("Digest Loop Fired");
+	// })
+
+	// $scope.$watch('onceCounter',function(newValue,oldValue){
+	// 	console.log("onceCounter old value: ", oldValue);
+	// 	console.log("onceCounter new value: ", newValue);
+	// });
+
+	// $scope.$watch('counter',function(newValue,oldValue){
+	// 	console.log("counter old value: ", oldValue);
+	// 	console.log("counter new value: ", newValue);
+	// });
+}
+
 })();
